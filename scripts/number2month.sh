c004a7ab09d9e389fe1f0453c62961338e76d3d7
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2021-11-13 16:44:15 +0100

    added .gitignore; fixed number2month.sh

--- scripts/number2month.sh
+++ scripts/number2month.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sed -i -e "s/month     = {1}/{jan}/" *.bib
-sed -i -e "s/month     = {2}/{feb}/" *.bib
-sed -i -e "s/month     = {3}/{mar}/" *.bib
-sed -i -e "s/month     = {4}/{apr}/" *.bib
-sed -i -e "s/month     = {5}/{may}/" *.bib
-sed -i -e "s/month     = {6}/{jun}/" *.bib
-sed -i -e "s/month     = {7}/{jul}/" *.bib
-sed -i -e "s/month     = {8}/{aug}/" *.bib
-sed -i -e "s/month     = {9}/{sep}/" *.bib
-sed -i -e "s/month     = {10}/{oct}/" *.bib
-sed -i -e "s/month     = {11}/{nov}/" *.bib
-sed -i -e "s/month     = {12}/{dez}/" *.bib
+sed -i -e "s/month     = {1}/month     = {jan}/" *.bib
+sed -i -e "s/month     = {2}/month     = {feb}/" *.bib
+sed -i -e "s/month     = {3}/month     = {mar}/" *.bib
+sed -i -e "s/month     = {4}/month     = {apr}/" *.bib
+sed -i -e "s/month     = {5}/month     = {may}/" *.bib
+sed -i -e "s/month     = {6}/month     = {jun}/" *.bib
+sed -i -e "s/month     = {7}/month     = {jul}/" *.bib
+sed -i -e "s/month     = {8}/month     = {aug}/" *.bib
+sed -i -e "s/month     = {9}/month     = {sep}/" *.bib
+sed -i -e "s/month     = {10}/month     = {oct}/" *.bib
+sed -i -e "s/month     = {11}/month     = {nov}/" *.bib
+sed -i -e "s/month     = {12}/month     = {dez}/" *.bib
