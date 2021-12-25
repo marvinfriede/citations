1929d109e179aef9baed61ca7955b6312bb0b1e0
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2021-12-25 13:22:27 +0100

    new folders for migration to `texmf`; recursive search for .bib files

--- scripts/month2number.sh
+++ scripts/month2number.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-sed -i -e "s/{[Jj]an}/{1}/" *.bib
-sed -i -e "s/{[Ff]eb}/{2}/" *.bib
-sed -i -e "s/{[Mm]ar}/{3}/" *.bib
-sed -i -e "s/{[Aa]pr}/{4}/" *.bib
-sed -i -e "s/{[Mm]ay}/{5}/" *.bib
-sed -i -e "s/{[Jj]un}/{6}/" *.bib
-sed -i -e "s/{[Jj]ul}/{7}/" *.bib
-sed -i -e "s/{[Aa]ug}/{8}/" *.bib
-sed -i -e "s/{[Ss]ep}/{9}/" *.bib
-sed -i -e "s/{[Oo]ct}/{10}/" *.bib
-sed -i -e "s/{[Nn]ov}/{11}/" *.bib
-sed -i -e "s/{[Dd]ez}/{12}/" *.bib
+shopt -s globstar
+
+sed -i -e "s/{[Jj]an}/{1}/" **/*.bib
+sed -i -e "s/{[Ff]eb}/{2}/" **/*.bib
+sed -i -e "s/{[Mm]ar}/{3}/" **/*.bib
+sed -i -e "s/{[Aa]pr}/{4}/" **/*.bib
+sed -i -e "s/{[Mm]ay}/{5}/" **/*.bib
+sed -i -e "s/{[Jj]un}/{6}/" **/*.bib
+sed -i -e "s/{[Jj]ul}/{7}/" **/*.bib
+sed -i -e "s/{[Aa]ug}/{8}/" **/*.bib
+sed -i -e "s/{[Ss]ep}/{9}/" **/*.bib
+sed -i -e "s/{[Oo]ct}/{10}/" **/*.bib
+sed -i -e "s/{[Nn]ov}/{11}/" **/*.bib
+sed -i -e "s/{[Dd]ez}/{12}/" **/*.bib
--- scripts/number2month.sh
+++ scripts/number2month.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-sed -i -e "s/month     = {1}/month     = {jan}/" *.bib
-sed -i -e "s/month     = {2}/month     = {feb}/" *.bib
-sed -i -e "s/month     = {3}/month     = {mar}/" *.bib
-sed -i -e "s/month     = {4}/month     = {apr}/" *.bib
-sed -i -e "s/month     = {5}/month     = {may}/" *.bib
-sed -i -e "s/month     = {6}/month     = {jun}/" *.bib
-sed -i -e "s/month     = {7}/month     = {jul}/" *.bib
-sed -i -e "s/month     = {8}/month     = {aug}/" *.bib
-sed -i -e "s/month     = {9}/month     = {sep}/" *.bib
-sed -i -e "s/month     = {10}/month     = {oct}/" *.bib
-sed -i -e "s/month     = {11}/month     = {nov}/" *.bib
-sed -i -e "s/month     = {12}/month     = {dez}/" *.bib
+shopt -s globstar
+
+sed -i -e "s/month     = {1}/month     = {jan}/" **/*.bib
+sed -i -e "s/month     = {2}/month     = {feb}/" **/*.bib
+sed -i -e "s/month     = {3}/month     = {mar}/" **/*.bib
+sed -i -e "s/month     = {4}/month     = {apr}/" **/*.bib
+sed -i -e "s/month     = {5}/month     = {may}/" **/*.bib
+sed -i -e "s/month     = {6}/month     = {jun}/" **/*.bib
+sed -i -e "s/month     = {7}/month     = {jul}/" **/*.bib
+sed -i -e "s/month     = {8}/month     = {aug}/" **/*.bib
+sed -i -e "s/month     = {9}/month     = {sep}/" **/*.bib
+sed -i -e "s/month     = {10}/month     = {oct}/" **/*.bib
+sed -i -e "s/month     = {11}/month     = {nov}/" **/*.bib
+sed -i -e "s/month     = {12}/month     = {dez}/" **/*.bib
